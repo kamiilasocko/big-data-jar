f04ba4814427bf5590464dc53652c749fb559e99
Kamila <kamilasocko1@gmail.com>
2022-06-15 23:04:07 +0200

    fix jar

--- src/main/scala/agh/wggios/analizadanych/Main.scala
+++ src/main/scala/agh/wggios/analizadanych/Main.scala
@@ -14,7 +14,7 @@ object Main extends SparkSessionProvider {
 
     import spark.implicits._
 
-    val flightsDf =new DataReader("flight-data.csv").read().as[FlightCaseClass]
+    val flightsDf =new DataReader(args(0)).read().as[FlightCaseClass]
     val newDf= flightsDf.filter(flight_row => new Transformations().originIsDestination(flight_row)).show()
 
     flightsDf.groupByKey(x => x.DEST_COUNTRY_NAME).reduceGroups((l, r) => new Transformations().sumOfFlight(l, r)).show()
